c3e7228bc940aa36ff526dffdf98f7ac226d1d91
amie-dev <learn.amie01@gmail.com>
2026-04-11 19:38:10 +0530

    upto auth final export

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import "dotenv/config";
 import authRouter from "./routers/authRouter.js";
 import { connectDB } from "./config/db.js";
+import cookieParser from "cookie-parser";
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
